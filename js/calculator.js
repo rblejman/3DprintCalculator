55823aa9a58acad61e94922edece0f5870be7280
rodrigo blejman <rodrigo.blej@gmail.com>
2021-11-12 18:20:19 -0300

    added show function and styles

--- js/calculator.js
+++ js/calculator.js
@@ -13,6 +13,79 @@ function calcPrint(
   let labourCost = time * labour;
   let totalCost = (energyCost + materialCost + labourCost) * (taxes / 100 + 1);
   let finalPrice = totalCost * (profit / 100 + 1);
+
+  $(".result").html("");
+  $(".result")
+    .html(
+      `
+  <table class="result__inPuts">
+      <tr>
+        <th>Impresora:</th>
+        <th class = "result__inputs-right">${
+          printers[$("#printerSelect").val()].model
+        }</th>
+      </tr>
+      <tr>
+        <th>Consumo:</th>
+        <th class = "result__inputs-right" >${
+          printers[$("#printerSelect").val()].consumption
+        } watts</th>
+      </tr>
+      <tr>
+        <th>Electricidad:</th>
+        <th class = "result__inputs-right" >$${energy.toFixed(2)} kWh</th>
+      </tr>
+      <tr>
+        <th>Tiempo:</th>
+        <th class = "result__inputs-right" >${time.toFixed(2)} min.</th>
+      </tr>
+      <tr>
+        <th>Material:</th>
+        <th class = "result__inputs-right" >$${material.toFixed(2)}</th>
+      </tr>
+      <tr>
+        <th>Peso:</th>
+        <th class = "result__inputs-right" >${weight.toFixed(2)}.grs</th>
+      </tr>
+      <tr>
+        <th>Mano de Obra:</th>
+        <th class = "result__inputs-right" >$${labour.toFixed(2)} /Hr</th>
+      </tr>
+      <tr>
+        <th>Impuestos:</th>
+        <th class = "result__inputs-right" >${taxes.toFixed(2)}%</th>
+      </tr>
+      <tr>
+        <th>Ganancia</th>
+        <th class = "result__inputs-right" >${profit.toFixed(2)}%</th>
+      </tr>
+    </table>
+    <br />
+    <table class="result__outPuts" >
+      <tr>
+        <th>Energía:</th>
+        <th class = "result__outPuts-right">$ ${energyCost.toFixed(2)}</th>
+      </tr>
+      <tr>
+        <th>Material</th>
+        <th class = "result__outPuts-right" >$ ${materialCost.toFixed(2)}</th>
+      </tr>
+      <tr>
+        <th>Labor:</th>
+        <th class = "result__outPuts-right" >$ ${labourCost.toFixed(2)}</th>
+      </tr>
+      <tr>
+        <th>Costo total:</th>
+        <th class = "result__outPuts-right" >$ ${totalCost.toFixed(2)}</th>
+      </tr>
+      <tr class = "result__outPuts-finalPrice" >
+        <th>Precio final:</th>
+        <th class = "result__outPuts-right" >$ ${finalPrice.toFixed(2)}</th>
+      </tr>
+    </table>
+  `
+    )
+    .show("slow");
 }
 
 // Objeto impresora. Su modelo y consumo. Metodo: calcular consumo por el tiempo de impresion. consumo en watts/1000 = Kwh. minutos/60 = horas
@@ -30,6 +103,7 @@ let printers = [
   new PrinterModel("Makerbot Replicator+", 150),
 ];
 
+$(".result").hide();
 $(".calcForm").submit((e) => {
   e.preventDefault();
   let printerConsumption = printers[$("#printerSelect").val()].consumption;
@@ -42,17 +116,6 @@ $(".calcForm").submit((e) => {
   let profitMarginPercent = parseInt($("#profitMarginPercent").val());
 
   calcPrint(
-    printerConsumption,
-    energyPriceInKwh,
-    printTimeInMinutes,
-    materialPricePerGram,
-    weightInGrams,
-    labourCostInMinutes,
-    taxes,
-    profitMarginPercent
-  );
-
-  console.log(
     printerConsumption,
     energyPriceInKwh,
     printTimeInMinutes,
